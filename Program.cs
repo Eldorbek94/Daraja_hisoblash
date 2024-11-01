152be436fd6208c417f579a4f6fd946d47de9a90
Eldorbek94 <eldorbtc@gmail.com>
2024-11-01 15:41:56 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// daraja xisoblash
+Console.WriteLine("Daraja xisoblash");
+Console.Write("Sonni kiriting : ");
+int son = Convert.ToInt32(Console.ReadLine());
+Console.Write("Darajani kiriting : ");
+int daraja = Convert.ToInt32(Console.ReadLine());
+int summ=1;
+for(int i=0;i<daraja;i++){
+    summ=summ*son;
+}
+
+Console.Write($"{son} sonining {daraja} - darajasi: {summ}");
